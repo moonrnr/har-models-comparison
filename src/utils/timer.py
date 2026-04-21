607f8edea00eaea1ac3add3f5e4950d20c5b1d2b
moonrnr <moonrnr@users.noreply.github.com>
2026-04-22 00:03:55 +0200

    Add data exploration notebook, add timer class

---
+++ src/utils/timer.py
@@ -0,0 +1,15 @@
+import time
+
+class Timer:
+    def __init__(self, label="block", verbose=False):
+        self.label = label
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.t0 = time.perf_counter()
+        return self
+
+    def __exit__(self, *exc):
+        self.elapsed_time = time.perf_counter() - self.t0
+        if self.verbose:
+            print(f"[{self.label}] time: {self.elapsed_time:.3f} s")
